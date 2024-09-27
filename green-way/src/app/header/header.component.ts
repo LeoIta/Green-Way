ae951881f5d858d046abb727af3235074d66028f
leoita <leonardolorusso83@gmail.com>
2024-09-28 01:11:52 +0200

    Responsive Header

---
+++ green-way/src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
+
+@Component({
+  selector: 'app-header',
+  standalone: false,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
