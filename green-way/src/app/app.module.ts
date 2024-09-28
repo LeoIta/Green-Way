7e1f740fdef499fdc62eeb3c3731acc12f8371d4
leoita <leonardolorusso83@gmail.com>
2024-09-29 00:03:20 +0200

    Responsive Footer

--- green-way/src/app/app.module.ts
+++ green-way/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
